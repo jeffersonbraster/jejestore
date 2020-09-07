a136a4e2c98642be8f51d880083e3a77c4f3dadd
Jefferson Brandão <jeffersonbraster@gmail.com>
2020-09-07 14:42:30 -0300

    Criando server node

---
+++ backend/data.js
@@ -0,0 +1,99 @@
+export default {
+    products: [
+    {
+        _id: '1',
+        name: 'Short Molezinha PRIME',
+        category: 'Shirts',
+        image: '/images/d1.jpg',
+        price: 100,
+        brand: 'Nike',
+        rating: 4.5,
+        numReviews: 10
+    },
+    {
+        _id: '2',
+        name: 'Short Fit',
+        category: 'Shirts',
+        image: '/images/d1.jpg',
+        price: 110,
+        brand: 'Nike',
+        rating: 4.5,
+        numReviews: 10
+    },
+    {
+        _id: '3',
+        name: 'Short Sport',
+        category: 'Shirts',
+        image: '/images/d1.jpg',
+        price: 120,
+        brand: 'Iury marcas',
+        rating: 4.5,
+        numReviews: 10
+    },
+    {
+        _id: '4',
+        name: 'Pants Casual',
+        category: 'Pants',
+        image: '/images/d1.jpg',
+        price: 130,
+        brand: 'Molezinhas marcas',
+        rating: 4.5,
+        numReviews: 10
+    },
+    {
+        name: 'Pants Regatron',
+        category: 'Pants',
+        image: '/images/d1.jpg',
+        price: 140,
+        brand: 'jeje marcas',
+        rating: 4.5,
+        numReviews: 10
+    },
+    
+    {
+        name: 'Pants Regatron',
+        category: 'Pants',
+        image: '/images/d1.jpg',
+        price: 140,
+        brand: 'jeje marcas',
+        rating: 4.5,
+        numReviews: 10
+    },
+    {
+        name: 'Pants Regatron',
+        category: 'Pants',
+        image: '/images/d1.jpg',
+        price: 140,
+        brand: 'jeje marcas',
+        rating: 4.5,
+        numReviews: 10
+    },
+    {
+        name: 'Pants Regatron',
+        category: 'Pants',
+        image: '/images/d1.jpg',
+        price: 140,
+        brand: 'jeje marcas',
+        rating: 4.5,
+        numReviews: 10
+    },
+    {
+        name: 'Pants Regatron',
+        category: 'Pants',
+        image: '/images/d1.jpg',
+        price: 140,
+        brand: 'jeje marcas',
+        rating: 4.5,
+        numReviews: 10
+    },
+    {
+        name: 'Pants Regatron',
+        category: 'Pants',
+        image: '/images/d1.jpg',
+        price: 140,
+        brand: 'jeje marcas',
+        rating: 4.5,
+        numReviews: 10
+    },
+]
+}
